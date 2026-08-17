387889576f3c4d12c4d56a8282fa89c32e37d2cc
Nisesimadao <opevista@gmail.com>
2026-08-17 10:22:48 +0900

    Prepare 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.cloudwalk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     buildTypes {
